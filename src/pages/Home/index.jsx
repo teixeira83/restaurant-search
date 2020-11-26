2baac629fa2fd5d8e7270cd72b7b53ff8a7c1600
teixeira83 <rafael.amtx@gmail.com>
2020-11-26 14:11:50 -0300

    Create aside Input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react'
+import logo from '../../assets/logo.svg'
+import { Container, Search } from './styles';
+import TextField, { Input } from '@material/react-text-field';
 
-const Home = () => <Container>Hello World!</Container>
+const Home = () => {
+    const [inputValue, setInputValue] = useState("");
+
+    return (
+        <Container>
+            <Search>
+                <img src={logo} alt="Logo do Restaurante"></img>
+                <TextField 
+                    label="Pesquisar"
+                    outlined
+                >
+                    <Input 
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                    />
+                </TextField>
+            </Search>
+        </Container>
+    )
+}
 
 export default Home;
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,9 +1,25 @@
 import styled from 'styled-components';
 
-export const Container = styled.div `
+export const Container = styled.aside`
     /*
     ** Exemplo de como utilizar um objeto theme
     ** background-color: ${(props) => props.theme.colors.primary};
     */
-   
+
+   background-color: ${(props) => props.theme.colors.background};
+   width: 360px;
+   height: 100vh;
+   overflow-y: auto;
 `;
+
+export const Search = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #ffffff;
+    padding: 16px;
+`;
+
+
+
+
